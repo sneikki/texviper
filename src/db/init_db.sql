f183a6ff103d2dc3ebe7f3f5643d89f56ee84208
Nuutti Varvikko <nvarvikko@gmail.com>
2021-04-18 15:17:59 +0300

    add timestamp

--- src/db/init_db.sql
+++ src/db/init_db.sql
@@ -1,5 +1,6 @@
 create table if not exists Projects (
-    project_id  varchar(36) primary key,
-    name        text unique not null,
-    path        text not null
+    project_id      varchar(36) primary key,
+    name            text unique not null,
+    path            text        not null,
+    last_modified   timestamp   not null
 );
