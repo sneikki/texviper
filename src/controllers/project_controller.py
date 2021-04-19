c09bf4bc60ddf5d48538724a371dc3013ca8892e
Nuutti Varvikko <nvarvikko@gmail.com>
2021-04-19 17:45:59 +0300

    refactor project controller and store

--- src/controllers/project_controller.py
+++ src/controllers/project_controller.py
@@ -1,28 +1,80 @@
 from models.project import Project
 from stores.project_store import project_store
-
+from utils.exceptions import ProjectExistsError
 
 class ProjectController:
     """ Handles logical operations for projects
     """
 
     def create_project(self, name, path):
-        """ Creates a new project
+        """ Creates a new project and saves it to database and file system
 
         Args:
-            name (string): String that represents name of the project
-            path (string): String that represents path of the project
+            name (string): string that represents name of the project
+            path (string): string that represents path of the project
         """
-
+        
+        if project_store.exists(name):
+            raise ProjectExistsError
         project = Project(name, path)
 
-        project_store.create_project(project)
+        project_store.create(project)
 
     def get_project_names(self):
+        """ Returns list of names of all projects
+
+        Returns:
+            list: list of names of all projects
+        """
+
         names = map(lambda name: name[0], project_store.find_all(['name']))
         return names
 
-    def remove_project(self, name):
-        project_store.remove_project_by_name(name)
+    def get_projects(self):
+        """ Returns list of all projects containing all fields
+
+        Returns:
+            list: list of all projects or empty list if nothing was found
+        """
+
+        projects = project_store.find_all()
+        return projects
+
+    def get_project_by_id(self, project_id):
+        """ Finds and returns a single project by project_id.
+            Returns None if no project with given project_id can be found.
+
+        Args:
+            project_id (string): Id of the project
+
+        Returns:
+            Project: project or None if nothing was found
+        """
+
+        project = project_store.find_one([ ('project_id', project_id) ])
+        return project
+
+    def get_project_by_name(self, name):
+        """ Finds and returns a single project by name.
+            Returns None if no project with given name can be found.
+
+        Args:
+            name (string): name of the project
+
+        Returns:
+            Project: project or None if nothing was found
+        """
+
+        project = project_store.find_one(['name'], [ ('name', name) ])
+        return project
+
+    def remove_project(self, project_id):
+        """ Deletes a project by project_id
+
+        Args:
+            project_id (string): id of the project to remove
+        """
+
+        project_store.delete_project(project_id)
 
 project_controller = ProjectController()
--- src/stores/project_store.py
+++ src/stores/project_store.py
@@ -1,114 +1,174 @@
 from pathlib import Path
-import shutil
-from sqlite3 import IntegrityError
+from sqlite3 import (
+    IntegrityError, OperationalError
+)
 
-from config.config import config
 from db.db_connection import database
-from utils.literal import literals
-
-class CreateProjectError(Exception):
-    pass
+from config.config import config
+from utils.exceptions import DirectoryExistsError
+from utils.filesystem import file_system
 
 class ProjectStore:
-    def __init__(self):
-        self.projectrc_name = "projectrc.json"  # for now
+    #
+    #   Public
+    #
 
-    def create_project(self, project):
-        path = Path(project.path) / project.name
-        self._create_record(project)
+    def create(self, project):
+        """ Creates a new project
+
+            Creates a new project by model from parameter project.
+            Inserts a record into database and writes appropriate
+            files to file system.
+
+        Args:
+            project (Project): project to be created
+        """
+
+        # A transaction is used to ensure that no record is
+        # created in case of file system operations fail.
+        database.begin()
 
         try:
-            self._create_directory(path)
-            self._init_project(project)
-        except CreateProjectError as err:
+            self._create_record(project)
+            self._write(project)
+        except (
+            PermissionError,
+            DirectoryExistsError,
+            IntegrityError,
+            OperationalError
+        ) as err:
             database.rollback()
             raise err
         else:
             database.commit()
 
-    def change_project(self, new_data):
-        pass
+    def exists(self, name):
+        """ Check wheter a project with a given name
+            exists in database.
 
-    def remove_project_by_id(self, project_id, remove_dir=True):
-        if remove_dir:
-            # Query project path and remove directory
+            Args:
+                name (string): name of the project the look for
 
-            query = f"""
-                select name, path from Projects
-                where project_id="{project_id}"
-            """
-            database.execute(query)
-            res = database.fetch_one()
-            name = res[0]
-            path = res[1]
-
-            # Exception about nonexistent directory can be safely ignored since
-            # nothing needs to be done if the directory has already been removed
-            try:
-                shutil.rmtree(Path(path) / name)
-            except Exception:
-                pass
-
-        query = f"""
-            delete from Projects
-            where project_id="{project_id}"
         """
 
-        database.execute(query)
-        database.commit()
-
-    def remove_project_by_name(self, name):
         query = f"""
             select project_id from Projects
             where name="{name}"
         """
 
         database.execute(query)
-        project_id = database.fetch_one()[0]
+        return len(database.fetch_all()) > 0
 
-        self.remove_project_by_id(project_id)
-
-    def find_one(self):
+    def delete_one(self):
         pass
 
-    def find_all(self, fields=None):
-        query_fields = ','.join(fields) if fields else '*'
+    def delete_many(self):
+        pass
+
+    def find_one(self, fields, conditions):
+        """ Looks for a single project by given conditions
+            and returns appropriate fields.
+
+        Args:
+            fields (list): List of fields to be queried
+            conditions (list): List of conditions for limiting the query
+        
+        Returns:
+            Project: project or None if nothing was found
+        """
 
         query = f"""
-            select {query_fields} from Projects
+            select {database.concatenate_fields(fields)} from Projects
+            where {database.construct_condition(conditions)}
+        """
+
+        database.execute(query)
+        return database.fetch_one()
+
+    def find_all(self, fields):
+        """ Fetches all projects from database and returns
+            appropriate fields.
+
+        Args:
+            fields (list): List of fields to be queried
+
+        Returns:
+            list: list of projects or empty list if nothing was found 
+
+        """
+
+        query = f"""
+            select {database.concatenate_fields(fields)} from Projects
         """
 
         database.execute(query)
         return database.fetch_all()
 
-    def _create_directory(self, path):
-        try:
-            path.mkdir(parents=True)
-        except PermissionError as permission_error:
-            raise CreateProjectError(literals['permission_denied']) from permission_error
-        except FileExistsError as exists_error:
-            raise CreateProjectError(literals['dir_exists']) from exists_error
-
-    def _init_project(self, project):
-        rc_path = Path(project.path) / project.name / self.projectrc_name
-
-        rc_path.touch()
-
-        project_rc = config.create_project_config(project)
-        rc_path.write_text(project_rc)
+    #
+    #   Private
+    #
 
     def _create_record(self, project):
-        # Use a transaction to ensure that no database record is
-        # created if creation of project files fails
-        query = f"""
-            begin;
-            insert into Projects (project_id, name, path, last_modified)
-            values ("{project.project_id}", "{project.name}", "{project.path}", "{project.last_modified}")
+        """ Inserts project data into database
+
+        Args:
+            project (Project): data to be inserted
+
         """
 
-        try:
-            database.execute_script(query)
-        except IntegrityError as err:
-            raise CreateProjectError(literals['project_already_exists']) from err
+        query = f"""
+            insert into Projects (project_id, name, path, last_modified)
+            values ("{project.project_id}", "{project.name}",
+            "{project.path}", "{project.last_modified}")
+        """
+
+        database.execute(query)
+
+    def _write(self, project):
+        """ Writes project files to file system.
+            Directory, if already existing, must
+            be empty. Otherwise an error will be
+            thrown.
+
+        Args:
+            project (Project): data to be written
+        """
+
+        expanded_path = Path(project.path).expanduser() / project.name
+
+        self._initialize_directory(expanded_path)
+        self._initialize_config(expanded_path, project.project_id, project.name)
+
+    def _initialize_directory(self, path):
+        """ Initializes project directory at desired location.
+            If existing directory is used, it must be empty.
+            Otherwise an exception will be raised
+
+        Args:
+            path (Path): Project path
+
+        Raises:
+            DirectoryExistsError: if existing directory is not empty
+        """
+
+        if file_system.directory_exists(path) and not file_system.directory_empty(path):
+                raise DirectoryExistsError()
+        
+        file_system.create_directory(path)
+
+    def _initialize_config(self, path, project_id, name):
+        """ Adds project configuration to project directory
+
+        Args:
+            path (Path): project path
+            project_id (string): id of the project to store in the config
+            name (string): name of the project to store in the config
+        """
+
+        config_str = config.create_project_config(project_id, name)
+        full_path = path / 'projectrc.json'
+
+        file_system.create_file(full_path)
+        file_system.write(full_path, config_str)
 
 project_store = ProjectStore()
